c37abdd05d8d99e34985bf13ecf26fb230e0a3e5
MagnusRef <magnusrc@protonmail.com>
2022-10-01 13:02:43 +0200

    node changes

--- terraform/cluster/vars.tf
+++ terraform/cluster/vars.tf
@@ -29,7 +29,7 @@ variable "vms" {
       hostname = "NS-K8SCPL-01"
       fqdn     = "ns-k8scpl-01.l1-nak.local"
       cpu      = 4
-      ram      = 4096
+      ram      = 2048
       ip       = "10.2.100.10/24"
       gw       = "10.2.100.1"
       dns1     = "10.2.90.10"
@@ -39,7 +39,7 @@ variable "vms" {
       hostname = "NS-K8SWRK-01"
       fqdn     = "ns-k8swrk-01.l1-nak.local"
       cpu      = 4
-      ram      = 8192
+      ram      = 2048
       ip       = "10.2.100.11/24"
       gw       = "10.2.100.1"
       dns1     = "10.2.90.10"
@@ -49,7 +49,7 @@ variable "vms" {
       hostname = "NS-K8SWRK-02"
       fqdn     = "ns-k8swrk-02.l1-nak.local"
       cpu      = 4
-      ram      = 8192
+      ram      = 2048
       ip       = "10.2.100.12/24"
       gw       = "10.2.100.1"
       dns1     = "10.2.90.10"
@@ -59,7 +59,7 @@ variable "vms" {
       hostname = "NS-K8SWRK-03"
       fqdn     = "ns-k8swrk-03.l1-nak.local"
       cpu      = 4
-      ram      = 8192
+      ram      = 2048
       ip       = "10.2.100.13/24"
       gw       = "10.2.100.1"
       dns1     = "10.2.90.10"
